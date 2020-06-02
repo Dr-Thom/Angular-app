b2c827ff80a0726f7f568bb4cd7986c76f4dc15e
drThom <drthomsa@gmail.com>
2020-06-02 14:01:09 -0400

    committing crud (not CRUD)

--- crud/src/app/app.module.ts
+++ crud/src/app/app.module.ts
@@ -3,18 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AvatarDialogComponent } from './avatar-dialog/avatar-dialog.component';
-import { HomeComponent } from './home/home.component';
-import { EditUserComponent } from './edit-user/edit-user.component';
-import { NewUserComponent } from './new-user/new-user.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    AvatarDialogComponent,
-    HomeComponent,
-    EditUserComponent,
-    NewUserComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
